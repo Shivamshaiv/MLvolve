dd5b310685e726f3325255f894a24b3955a2ae80
Shivamshaiv <patelshivam99@gmail.com>
2020-01-23 16:55:46 +0000

    Heavily modified the Student class and made it aligned with the junior researcher class.

--- Agents/researchers.py
+++ Agents/researchers.py
@@ -1,25 +1,32 @@
 import numpy as np
 from mesa import Agent, Model
-from mesa.time import RandomActivation
+from mesa.time import RandomActivation,SimultaneousActivation,StagedActivation
 import torch
 import pyro
 import torch.distributions as tod
 import pyro.distributions as pyd
 import matplotlib.pyplot as plt
+from quantecon.distributions import BetaBinomial
+from scipy.stats import poisson
+import plotly.graph_objects as go
+%matplotlib inline
+
 
 
 class Student(Agent):
-    """An agent with fixed initial wealth."""
+    """An agent who is a university student."""
     def __init__(self,unique_id,model):
         super().__init__(unique_id,model)
-        self.unique_id = len(model.schedule.agents) + 1
-        self.category = 'S'   # Student Category
+        self.model = model
+        self.unique_id = 'U_' + str(len(model.schedule.agents) + 1)
+        self.category = 'U'   # University Student Category
         self.iq = self.iq_gen()
         self.ambitions = self.goal_generator()
         self.location = self.location_generator()
-        self.publications = 0
-        self.citations = 0
-        self.reputation = -1
+        self.publications = self.gen_publications()
+        self.citations = self.gen_citations()
+        self.reputation_points = 0
+        #self.reputation = self.compute_reputation(model)
 
     def iq_gen(self):
       '''
@@ -59,66 +66,72 @@ class Student(Agent):
       location = 'eu' if loca.item() == 1.0 else 'am'
       return location
 
-    def step(self):
-      print(self.unique_id)
-
-
-
-class Junior(Agent):
-  """An agent who is a junior researcher PhD, postdoc , early Researchers"""
-      def __init__(self,unique_id,model,promoted_attrs = None):
-          super().__init__(unique_id,model)
-          self.unique_id = 'J_' + str(len(model.schedule.agents) + 1)
-          self.category = 'J'   # Junior Researcher Category
-          self.numtopics = 5
-          if promoted_attrs:
-            self.iq = promoted_attrs['iq']
-            self.location = promoted_attrs['location']
-            self.publications =  promoted_attrs['publications']
-            self.citations = promoted_attrs['citations']
-            self.ambitions = promoted_attrs['self.ambitions']
-            
-            self.researh_exp = self.exp_gen()
-            self.reputation_points = self.gen_reputation_points()
-            self.reputation = self.compute_reputation()
-            self.topic_interested = self.select_topic()                   # We assume that there are 5 topics
-
-  def namegen(self,prefix):
+    def namegen(self,prefix):
       return prefix + "_" + str(self.unique_id)
-  
-  def exp_gen(self):
-      uni_research = pyro.sample(self.namegen("research_exp"),pyd.Poisson(self.iq/100.))       # The research experience is a Possion Distrubution of 1/100th of thier iq
 
-  def gen_reputation_points(self):
+    def gen_publications(self):
+      num_pub = pyro.sample(self.namegen("number_publication"),pyd.Poisson(self.iq/100.))
+      return num_pub.item()
+
+    def gen_citations(self):
+      h_index_computed = pyro.sample(self.namegen("h_index"),pyd.Normal(3,0.3))
+      h_index = h_index_computed.item()
+      if h_index < 0:
+        h_index = 0
+      return h_index*self.publications
+
+    def gen_reputation_points(self,model):
       '''
       Reputation points are computed every tick and weighted sum of your citations, research experience and imperfectly your IQ
       '''
       w_citations = 1.5
-      w_research_expeirence = 1.2
+      w_publications = 1.2
       w_iq = pyro.sample(self.namegen("how_iq_matters"),pyd.Normal(1,0.05)).item()
-      rep_points = (w_citations*self.citations + w_research_expeirence*self.research_exp + w_iq*self.iq)/(w_citations+w_research_expeirence+w_iq)
+
+      agent_list = model.schedule.agents
+      cit_list = [agent.citations for agent in agent_list if agent.category == 'U']
+      pub_list = [agent.publications for agent in agent_list if agent.category == 'U']
+      iq_list  = [agent.iq for agent in agent_list if agent.category == 'U']
+
+      rep_points = (w_citations*(self.citations/max(cit_list))+ w_publications*(self.publications/max(pub_list)) + w_iq*(self.iq/max(iq_list)))/(w_citations+w_publications+w_iq)
       return rep_points
 
-  def compute_reputation(self,model):
+
+    def compute_reputation(self,model):
       agent_list = model.schedule.agents
       rep_list = []
       for agent in agent_list:
-        if agent.type == 'J':
+        if agent.category == 'U':
           rep_list.append(agent.reputation_points)
       sorted_rep = sorted(rep_list)
-      return sorted_rep.index(self.iq)
+      return sorted_rep.index(self.reputation_points)
 
-  def selelct_topic(self):
-      k = self.numtopics = 5
-      return pyro.sample(self.namegen("topic_select"),pyd.Categorical(torch.tensor([ 1/k ]*k))).item()
-    
+
+    def step_stage_1(self):
+      self.reputation_points = self.gen_reputation_points(self.model)
+
+    def step_stage_2(self):
+      self.reputation = self.compute_reputation(self.model)
+
+    def step_stage_final(self):
+      print(self.unique_id,"has a reputation of ",self.reputation,"because thier points are",self.reputation_points)
+
+    def step(self):
+      pass
+      #self.reputation_points = self.gen_reputation_points(self.model)
+      #self.reputation = self.compute_reputation(self.model)
+
+    def advance(self):
+      print(self.unique_id,"has a reputation of ",self.reputation,"because thier points are",self.reputation_points)
+
 
 
 class WorldModel(Model):
     """A model with some number of agents."""
     def __init__(self, N):
         self.num_agents = N
-        self.schedule = RandomActivation(self)
+        model_stages = ["step_stage_1", "step_stage_2","step_stage_final"]
+        self.schedule = StagedActivation(self,model_stages)
         # Create agents
         for i in range(self.num_agents):
             a = Student(i,self)
